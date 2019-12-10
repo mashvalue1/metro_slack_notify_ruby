e6f688d3e3b9fcc5ab224ca719f57f81c622e42c
mashvalue1 <opods333@gmail.com>
2019-12-10 23:57:30 +0900

    路線名のリクエストパラメータを駅名で呼び出せるようにyamlを作成

--- function.rb
+++ function.rb
@@ -1,17 +1,27 @@
 require "net/http"
 require 'json'
+require "yaml"
 
 METRO_ENDPOINT = "https://api.tokyometroapp.jp/api/v2"
+LINE_PARAMS = YAML.load_file(File.join(__dir__, "./line_params.yml"))
+
+def lambda_handler(event:, context:)
+  line_name = ENV["TARGET_LINE"]
+  line_info = get_line_info(line_name)
+end
 
 def get_line_info(line_name = nil)
-    q = {
-        "rdf:type": "odpt:TrainInformation",
-        "acl:consumerKey": ENV["TOKYO_METRO_ACCESS_KEY"]
-    }
-    q["odpt:railway"] = line_name if line_name
+  q = {
+      "rdf:type": "odpt:TrainInformation",
+      "acl:consumerKey": "ENV['TOKYO_METRO_ACCESS_KEY']"
+  }
 
-    uri = URI.parse(METRO_ENDPOINT + "/datapoints")
-    uri.query = URI.encode_www_form(q)
+  if LINE_PARAMS.keys.include? line_name
+    q["odpt:railway"] = LINE_PARAMS[line_name]
+  end
 
-    JSON.parse(Net::HTTP.get(uri))
+  uri = URI.parse(METRO_ENDPOINT + "/datapoints")
+  uri.query = URI.encode_www_form(q)
+  JSON.parse(Net::HTTP.get(uri))
 end
+
